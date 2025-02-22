f1bcf1513f59b1ef306695242b3f6288b5c19064
Dmitri Lishko <dmitrilishko@gmail.com>
2025-02-22 14:44:15 -0700

    task5: complete

--- 5_Search_in_Rotated_Sorted_Array/script.ts
+++ 5_Search_in_Rotated_Sorted_Array/script.ts
@@ -1,31 +1,32 @@
 export {};
-let nums: number[] = [5, 1, 3];
-let target: number = 5;
+let nums: number[] = [3, 5, 6, 0, 1, 2];
+let target: number = 4;
 
 const search = (nums: number[], target: number): number => {
   let leftIdx = 0;
   let rightIdx = nums.length - 1;
 
   while (leftIdx <= rightIdx) {
-    let midIdx = Math.floor((leftIdx + rightIdx) / 2);
-    // console.log(111111, midIdx);
+    let midIdx = Math.floor((rightIdx + leftIdx) / 2);
+
     if (nums[midIdx] === target) return midIdx;
-    if (nums[leftIdx] < nums[midIdx]) {
-      if (target > nums[leftIdx] && target < nums[midIdx]) {
-        rightIdx = midIdx;
+
+    if (nums[leftIdx] <= nums[midIdx]) {
+      if (target < nums[midIdx] && target >= nums[leftIdx]) {
+        rightIdx = midIdx - 1;
       } else {
         leftIdx = midIdx + 1;
       }
     } else {
-      if (target < nums[leftIdx] && target < nums[midIdx]) {
-        rightIdx = midIdx;
-      } else if (target < nums[leftIdx] && target > nums[midIdx]) {
-      } else {
+      if (target > nums[midIdx] && target <= nums[rightIdx]) {
         leftIdx = midIdx + 1;
+      } else {
+        rightIdx = midIdx - 1;
       }
     }
   }
+
   return -1;
 };
 
-console.log(999999, search(nums, target));
+search(nums, target);
